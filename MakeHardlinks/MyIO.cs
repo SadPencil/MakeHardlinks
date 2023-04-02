082750d665dc0f5efde480da0519ecb18ec780cb
Sad Pencil <sadpencil@outlook.com>
2023-04-02 23:48:38 +0800

    Upload initial codes

---
+++ MakeHardlinks/MyIO.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace MakeHardlinks
+{
+    public static class MyIO
+    {
+        /// <summary>
+        /// Delete everything in the directory.
+        /// </summary>
+        /// <param name="directory">The directory to be cleared</param>
+        public static void ClearDirectory(string directory)
+        {
+
+            var directoryInfo = new DirectoryInfo(directory);
+            foreach (var file in directoryInfo.GetFiles("*", SearchOption.TopDirectoryOnly))
+            {
+                file.Delete();
+            }
+            foreach (var folder in directoryInfo.GetDirectories("*", SearchOption.TopDirectoryOnly))
+            {
+                folder.Delete(true);
+            }
+        }
+        /// <summary>
+        /// Determine whether the two files are actually the same.
+        /// Note that in the ReFS file system, the method might not work properly. Must use GetFileInformationByHandleEx() to support ReFS.
+        /// </summary>
+        /// <param name="fileA">A file's path.</param>
+        /// <param name="fileB">Another file's path</param>
+        /// <returns></returns>
+        public static bool IsSameFile(string fileA, string fileB)
+        {
+            var fileAHandle = NativeMethods.CreateFileW(
+                fileA,
+                NativeConstants.GENERIC_READ,
+                NativeConstants.FILE_SHARE_READ | NativeConstants.FILE_SHARE_WRITE,
+                IntPtr.Zero,
+                NativeConstants.OPEN_EXISTING,
+                NativeConstants.FILE_ATTRIBUTE_NORMAL,
+                IntPtr.Zero
+                );
+
+            if (fileAHandle == NativeConstants.INVALID_HANDLE_VALUE)
+            {
+                Trace.WriteLine("[Warn] Can not open file " + fileA + ". Error " + NativeMethods.GetLastError() + ".");
+                return false;
+            }
+
+            var fileBHandle = NativeMethods.CreateFileW(
+                fileB,
+                NativeConstants.GENERIC_READ,
+                NativeConstants.FILE_SHARE_READ | NativeConstants.FILE_SHARE_WRITE,
+                IntPtr.Zero,
+                NativeConstants.OPEN_EXISTING,
+                NativeConstants.FILE_ATTRIBUTE_NORMAL,
+                IntPtr.Zero
+                );
+
+            if (fileBHandle == NativeConstants.INVALID_HANDLE_VALUE)
+            {
+                Trace.WriteLine("[Warn] Can not open file " + fileB + ". Error " + NativeMethods.GetLastError() + ".");
+                return false;
+            }
+
+            if (!NativeMethods.GetFileInformationByHandle(fileAHandle, out var fileAInfo))
+            {
+                Trace.WriteLine("[Warn] Can not get information of file " + fileA + ". Error " + NativeMethods.GetLastError() + ".");
+                return false;
+            }
+
+            if (!NativeMethods.GetFileInformationByHandle(fileBHandle, out var fileBInfo))
+            {
+                Trace.WriteLine("[Warn] Can not get information of file " + fileB + ". Error " + NativeMethods.GetLastError() + ".");
+                return false;
+            }
+
+            _ = NativeMethods.CloseHandle(fileAHandle);
+            _ = NativeMethods.CloseHandle(fileBHandle);
+
+            return (fileAInfo.dwVolumeSerialNumber == fileBInfo.dwVolumeSerialNumber) && (fileAInfo.nFileIndexHigh == fileBInfo.nFileIndexHigh) && (fileAInfo.nFileIndexLow == fileBInfo.nFileIndexLow);
+
+        }
+
+        /// <summary>
+        /// Delete all empty subfolders.
+        /// </summary>
+        /// <param name="directory">The folder</param>
+        /// <returns> Whether the folder is deleted or not.</returns>
+        public static bool RemoveEmptyFolders(string directory)
+        {
+            bool status = true;
+            foreach (string folder in Directory.GetDirectories(directory, "*", SearchOption.TopDirectoryOnly))
+            {
+                bool result = RemoveEmptyFolders(folder);
+                status = status && result;
+            }
+
+            if (status && (Directory.GetFiles(directory, "*", SearchOption.TopDirectoryOnly).Count() == 0))
+            {
+                Directory.Delete(directory);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Create hard links for every files, just like copying folders.
+        /// </summary>
+        /// <param name="srcDirectory">The source folder.</param>
+        /// <param name="destDirectory">The destination folder.</param>
+        /// <param name="override">Whether override files if exist</param>
+        /// <param name="disallowedExtensions"></param>
+        /// <param name="allowedExtensions"></param>
+        /// <param name="hardLinkCallback"></param>
+        /// <param name="copyFileCallback"></param>
+        public static void CreateHardLinksOfFiles(
+            string srcDirectory, string destDirectory,
+            bool @override = false, List<string> disallowedExtensions = null, List<string> allowedExtensions = null,
+            Action<string, string> hardLinkCallback = null, Action<string, string> copyFileCallback = null, Action<string, string> createDirectoryCallback = null)
+        {
+            Debug.WriteLine("Source: " + srcDirectory);
+
+            if (!Directory.Exists(destDirectory))
+            {
+                _ = Directory.CreateDirectory(destDirectory);
+                createDirectoryCallback?.Invoke(srcDirectory, destDirectory);
+            }
+
+            //Create all folders
+            foreach (string subDirectory in Directory.GetDirectories(srcDirectory, "*", SearchOption.AllDirectories))
+            {
+                string relativeName = subDirectory.Substring(srcDirectory.Length + 1);
+                //no matter this folder exists or not, the following method don't throw exception 
+
+                if (!Directory.Exists(Path.Combine(destDirectory, relativeName)))
+                {
+                    _ = Directory.CreateDirectory(Path.Combine(destDirectory, relativeName));
+                    createDirectoryCallback?.Invoke(Path.Combine(srcDirectory, relativeName), Path.Combine(destDirectory, relativeName));
+                }
+
+                Debug.WriteLine("CreateDirectory: " + relativeName);
+            }
+
+            //Create NTFS hard link if not existed
+            foreach (string srcFullName in Directory.GetFiles(srcDirectory, "*", SearchOption.AllDirectories))
+            {
+                string relativeName = srcFullName.Substring(srcDirectory.Length + 1);
+                string destFullName = Path.Combine(destDirectory, relativeName);
+
+                CreateHardLinkOrCopy(destFullName, srcFullName, @override, disallowedExtensions, allowedExtensions, hardLinkCallback, copyFileCallback);
+            }
+        }
+
+        public static void CreateHardLinkOrCopy(string destFile, string srcFile,
+            bool @override, List<string> disallowedExtensions, List<string> allowedExtensions,
+            Action<string, string> hardLinkCallback = null, Action<string, string> copyFileCallback = null)
+        {
+            string ext = Path.GetExtension(destFile).ToUpperInvariant();
+            if (File.Exists(destFile) && @override)
+            {
+                Debug.WriteLine("Overrided: " + destFile);
+                File.Delete(destFile);
+            }
+
+            if (!File.Exists(destFile))
+            {
+                bool hardlink = (allowedExtensions == null || allowedExtensions.Contains(ext)) &&
+                    (disallowedExtensions == null || !disallowedExtensions.Contains(ext));
+                if (hardlink)
+                {
+                    hardLinkCallback?.Invoke(srcFile, destFile);
+                    bool success = NativeMethods.CreateHardLinkW(destFile, srcFile, IntPtr.Zero);
+                    if (!success)
+                    {
+                        uint error = NativeMethods.GetLastError();
+                        throw new Exception($"Failed to make a hardlink from {srcFile} to {destFile}. Error code {error}.");
+                    }
+                }
+                else
+                {
+                    copyFileCallback?.Invoke(srcFile, destFile);
+                    File.Copy(srcFile, destFile, @override);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ MakeHardlinks/Program.cs
@@ -0,0 +1,77 @@
+﻿using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace MakeHardlinks
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // these variables will be set when the command line is parsed
+            int verbosity = 0;
+            bool shouldShowHelp = false;
+            var allowedExt = new List<string>();
+            var disallowedExt = new List<string>();
+            // these are the available options, note that they set the variables
+            var options = new OptionSet {
+                { "allow=", "only make hardlinks for files with given extensions; copy other files, e.g., --allow=.exe --allow=.png", n => allowedExt.Add (n.ToUpperInvariant()) },
+                { "disallow=", "copy and don't make hardlinks for files with given extensions, e.g., --disallow=.ini --disallow=.conf", n=> disallowedExt.Add (n.ToUpperInvariant()) },
+                { "v|verbose", "print filepath when processing every file", v => { if (v != null) ++verbosity; } },
+                { "h|help", "show this message and exit", h => shouldShowHelp = h != null },
+            };
+
+            string srcFolder;
+            string dstFolder;
+            try
+            {
+                // parse the command line
+                List<string> extra = options.Parse(args);
+                srcFolder = extra[0];
+                dstFolder = extra[1];
+
+                // extensions must start with dot
+                foreach (string ext in allowedExt.Concat(disallowedExt))
+                {
+                    if (!ext.StartsWith(".")) throw new Exception("Extensions must start with a dot.");
+                }
+
+                if (allowedExt.Count == 0) allowedExt = null;
+                if (disallowedExt.Count == 0) disallowedExt = null;
+            }
+            catch (Exception e)
+            {
+                // output some error message
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `--help' for more information.");
+                return;
+            }
+
+            if (shouldShowHelp)
+            {
+                Console.WriteLine("Help:");
+                Console.WriteLine("[arguments] <src_dir> <dest_dir>");
+                foreach (var option in options)
+                {
+                    Console.WriteLine($"{option.Prototype}\t\t{option.Description}");
+                }
+            }
+
+            try
+            {
+                MyIO.CreateHardLinksOfFiles(srcFolder, dstFolder, true, disallowedExt, allowedExt,
+                    (src, dst) => { if (verbosity >= 1) Console.WriteLine($"Make a hard link from {src} to {dst}."); },
+                    (src, dst) => { if (verbosity >= 1) Console.WriteLine($"Copy a file from {src} to {dst}."); },
+                    (src, dst) => { if (verbosity >= 1) Console.WriteLine($"Create a directory at {dst}."); }
+                );
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
